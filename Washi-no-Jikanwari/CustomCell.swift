3251657885c6655032e847f1cac1590e929cc71b
shogotaguchi <tagu00320@gmail.com>
2021-11-17 21:52:25 +0900

    修正

---
+++ Washi-no-Jikanwari/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  Washi-no-Jikanwari
+//
+//  Created by Shogo Taguchi on 2021/11/09.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    @IBOutlet weak var subjectLabel: UILabel!
+    
+    
+    @IBOutlet weak var roomLabel: UILabel!
+    
+    
+}
